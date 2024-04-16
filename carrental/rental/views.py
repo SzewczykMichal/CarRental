6bbfc1c4d2ac16ca4f632bc154268829b05abcb6
SzewczykMichal <13821@eppuz.edu.pl>
2024-04-16 19:13:48 +0200

    2024-04-03 MS

--- carrental/rental/views.py
+++ carrental/rental/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html.jinja')
+
+def cars(request):
+    return render(request,'cars.html.jinja')
+
+def car(request,car_id):
+    return render(request,'car.html.jinja')
